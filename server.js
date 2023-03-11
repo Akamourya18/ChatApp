527beafbe9e0523701431dbe2b601c8471c4872d
Akash Mourya <mouryakash883@gmail.com>
2023-03-12 01:04:25 +0530

    using socket

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socket.io(server);
 
 //Set static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
