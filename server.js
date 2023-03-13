6ca67cf60b0e36b926040bf4b9d800fe3a5c74c4
Akash Mourya <mouryakash883@gmail.com>
2023-03-14 01:17:32 +0530

    setting up socket

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socket.io(server);
+const io = socketio(server);
 
 //Set static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  //Welcome current user
+  socket.emit('message', 'Welcome to Lets Chat');
+
+  //Broadcast when a user connects
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  //Runs when a client disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  //Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  });
+});
 
 const PORT = 3000 || process.env.PORT;
 
