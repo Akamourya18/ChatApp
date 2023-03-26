270a73a5cbddfdde381e83de8bfca4a58d6f9e5b
Akash Mourya <mouryakash883@gmail.com>
2023-03-27 00:50:15 +0530

    Minor changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'LetsChat Bot';
 
 // Run when client connects
 io.on('connection', (socket) => {
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+    socket.emit('message', formatMessage(botName, 'Welcome to LetsChat!'));
 
     // Broadcast when a user connects
     socket.broadcast
